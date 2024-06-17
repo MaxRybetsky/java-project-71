fc512181175e2e46f36335540039f9ac768154e3
Maxim Rybetsky <stsdewar@ya.ru>
2024-06-17 17:22:59 +0300

    Step 6. Add tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,11 +2,39 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
     @Test
-    public void test() {
-        assertTrue(true);
+    public void whenGivenTwoJsonFilesThenGenerateDifferencesBetweenThem() throws URISyntaxException, IOException {
+        // given
+        String file1 = getAbsoluteFilePath("fixtures/given_file1.json");
+        String file2 = getAbsoluteFilePath("fixtures/given_file2.json");
+        String expected = readFile("fixtures/expected_json.txt");
+
+        // when
+        String result = Differ.generate(file1, file2);
+
+        // then
+        assertEquals(expected, result);
     }
+
+    private String getAbsoluteFilePath(String path) throws URISyntaxException {
+        return Paths.get(getClass().getClassLoader().getResource(path).toURI()).toString();
+    }
+
+    private String readFile(String path) throws IOException {
+        return new String(
+                getClass()
+                        .getClassLoader()
+                        .getResourceAsStream(path)
+                        .readAllBytes()
+        );
+    }
+
+
 }
